e071f3b491dd3b7d6a722a542dd082b0b072bfc3
Ryan <cybervoid41@gmail.com>
2024-12-27 06:23:55 -0700

    initial commit

---
+++ VulgarLangHelper/VulgarLangHelper/ViewModels/DictionaryEditorViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VulgarLangHelper.ViewModels
+{
+    public class DictionaryEditorViewModel
+    {
+        public string Title { get; set; } = "Dictionary Editor";
+    }
+}
---
+++ VulgarLangHelper/VulgarLangHelper/ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VulgarLangHelper.ViewModels
+{
+    public class HomeViewModel
+    {
+        public string Title { get; set; } = "Vulgar Lang Helper Home";
+    }
+}
